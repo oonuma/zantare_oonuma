e88e66bb4efc7b5bd7f677824f33b99f35aa5fba
oonuma <oonumaster@gmail.com>
2010-10-04 17:20:04 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZantareOonuma::Application.config.secret_token = '6f5d14241efef8f2e7076e7b892c61be8d9092d645dcf9f8d8661bec60c08edb546a41246415c575d23c5c9041877fceb5384b5f4da9617f016efb5f01174d29'
